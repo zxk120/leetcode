5531e7f06a9144f0ff453c261ab52cd2d7ea15fd
zxk120 <2096563280@qq.com>
2020-12-26 00:50:42 +0800

    Create answer.java

---
+++ 28_移除元素/answer.java
@@ -0,0 +1,15 @@
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/remove-element/solution/yi-chu-yuan-su-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
